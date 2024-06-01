d545d4a04756d5702d3104fa9b5b311fec78ae2b
senseiGai <swerss564@gmail.com>
2024-06-01 19:21:50 +0500

    BEST SELLERS DONE

--- src/widgets/HomePage/Mobile/BestSellersMobile/index.tsx
+++ src/widgets/HomePage/Mobile/BestSellersMobile/index.tsx
@@ -4,11 +4,11 @@ import React from "react";
 import Button from "@shared/ui/Button";
 import { MiniText } from "@shared/ui/MiniText";
 import { AboutCard } from "@entities/AboutCard";
+import Image from "next/image";
 import { content } from "@shared/lib/content/aboutCardContent";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
-
 
 import styles from "./styles.module.scss";
 
@@ -16,44 +16,53 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
+import bike from '@assets/webp/mob/best_sellers_mob.webp'
+
 export const BestSellersMobile = () => {
   return (
     <section className={styles.best_sellers_mob}>
-        <div className={styles.best_sellers_mob__text}>
-        <MiniText text="Best Sellers" />
-            <h3 className='mt-4'>
-                <strong>Your Business, Your Cart, Your Way</strong>
-            </h3>
-            <p className={styles.best_sellers_mob__text_paragraph}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </p>
+        <div className={styles.best_sellers_mob__content}>
+            <div className={styles.best_sellers_mob__content__text}>
+                <MiniText text="Best Sellers" />
+                <h3 className='mt-4'>
+                    <strong>Your Business, Your Cart, Your Way</strong>
+                </h3>
+                <p className={styles.best_sellers_mob__content__text_paragraph}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+            </div>
+        <div className={styles.best_sellers_mob__content__swiper}>
+            <Swiper
+            spaceBetween={20}
+            slidesPerView={1}
+            className={styles.best_sellers_mob__content__swiper_container}
+            navigation={false}
+        >
+            {content.map((item, index) => (
+            <SwiperSlide
+                key={index}
+            >
+                <AboutCard
+                name={item.name}
+                icon={item.icon}
+                text={item.text}
+                />
+            </SwiperSlide>
+            ))}
+            </Swiper>
         </div>
-        <div className={styles.best_sellers_mob__swiper}>
-        <Swiper
-        spaceBetween={20}
-        slidesPerView={1}
-        className={styles.best_sellers_mob__swiper_container}
-        navigation={false}
-      >
-        {content.map((item, index) => (
-          <SwiperSlide
-            key={index}
-          >
-            <AboutCard
-              name={item.name}
-              icon={item.icon}
-              text={item.text}
+            <Button
+            text="Manage Your Business"
+            buttonType="transparent"
+            margin="mt-8"
+            icon={faChevronRight}
             />
-          </SwiperSlide>
-        ))}
-        </Swiper>
         </div>
-        <Button
-          text="Manage Your Business"
-          buttonType="transparent"
-          margin="mt-8"
-          icon={faChevronRight}
-        />
+    <Image
+        src={bike}
+        alt="Best Sellers"
+        className={styles.best_sellers_mob__image}
+      />
     </section>
   )
 }
