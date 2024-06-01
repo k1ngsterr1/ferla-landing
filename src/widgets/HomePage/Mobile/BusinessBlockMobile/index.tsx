9febc91b8c2df0db41aa47d7e99d1cc453d61222
senseiGai <swerss564@gmail.com>
2024-06-01 22:18:58 +0500

    COSMETIC CHANGES

--- src/widgets/HomePage/Mobile/BusinessBlockMobile/index.tsx
+++ src/widgets/HomePage/Mobile/BusinessBlockMobile/index.tsx
@@ -20,26 +20,28 @@ export const BusinessBlockMobile = () => {
         alt="Ferla Bikes Main Image"
         className={styles.business_block_mob__image}
         />
-    <div className={styles.business_block_mob__text}>
-      <MiniText text="Aesthetic Vending Solutions" />
-        <h2 className='mt-4'>
-            Perfect <strong className="text-red">for Any Business</strong>
-        </h2>
-        <p className={styles.business_block_mob__text_paragraph}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.. Lorem ipsum dolor sit amet.
-        </p>
-    </div>
-    <div className={styles.business_block_mob__tabs}>
-      {tabs.map((tab) => 
-        <SolutionTab key={tab.name} name={tab.name} href={tab.href}/>
-      )}
-    </div>
-    <Button
-      text="Get Started"
-      buttonType="transparent"
-      margin="mt-4"
-      icon={faChevronRight}
-    />
+    <div className={styles.business_block_mob__content}>
+      <div className={styles.business_block_mob__content__text}>
+        <MiniText text="Aesthetic Vending Solutions" />
+          <h2 className='mt-4'>
+              Perfect <strong className="text-red">for Any Business</strong>
+          </h2>
+          <p className={styles.business_block_mob__content__text_paragraph}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.. Lorem ipsum dolor sit amet.
+          </p>
+      </div>
+      <div className={styles.business_block_mob__content__tabs}>
+        {tabs.map((tab) => 
+          <SolutionTab key={tab.name} name={tab.name} href={tab.href}/>
+        )}
+      </div>
+      <Button
+        text="Get Started"
+        buttonType="transparent"
+        margin="mt-4"
+        icon={faChevronRight}
+      />
+      </div>
     </section>
   )
 }
