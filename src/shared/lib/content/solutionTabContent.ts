02b489af0dbec6c48b054202777933112c304f1d
senseiGai <swerss564@gmail.com>
2024-06-01 15:43:33 +0500

    BUSINESS BLOCK DONE

---
+++ src/shared/lib/content/solutionTabContent.ts
@@ -0,0 +1,18 @@
+export const tabs = [
+    {
+        name: 'Cafe-On-Wheels',
+        href: 'AAA'
+    },
+    {
+        name: 'Pop-up Store',
+        href: 'AAA'
+    },
+    {
+        name: 'Marketing Campaign',
+        href: 'AAA'
+    },
+    {
+        name: 'Addition to Brick & Mortar',
+        href: 'AAA'
+    }
+]
