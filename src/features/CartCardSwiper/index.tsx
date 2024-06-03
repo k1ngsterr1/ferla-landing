0f4fc2f65cab8b7d8564f071ac47951ad66fad97
senseiGai <swerss564@gmail.com>
2024-06-03 12:16:57 +0500

    OUR BIKES BLOCK DONE

---
+++ src/features/CartCardSwiper/index.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { CartCard } from "@entities/CartCard";
+import { cartsContent } from "@shared/lib/content/cartsContent";
+
+import styles from "./styles.module.scss";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+export const CartCardSwiper  = () => {
+  return (
+    <Swiper
+      spaceBetween={20}
+      slidesPerView={1}
+      className={styles.swiper}
+      navigation={false}
+    >
+      {cartsContent.map((card, index) => (
+        <SwiperSlide key={index}>
+          <CartCard
+            name={card.name}
+            photo={card.image}
+            href={card.href}
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
--- src/widgets/HomePage/Mobile/OurBikesMobile/index.tsx
+++ src/widgets/HomePage/Mobile/OurBikesMobile/index.tsx
@@ -1,36 +1,24 @@
+
 import React from "react";
 import Button from "@shared/ui/Button";
 import Image from "next/image";
-import { CartCard } from "@entities/CartCard";
-import { cartsContent } from "@shared/lib/content/cartsContent";
+import { CartCardSwiper } from "@features/CartCardSwiper";
 
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 import styles from "./styles.module.scss";
 
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 export const OurBikesMobile = () => {
   return (
     <section className={styles.our_bikes_mob}>
-                <h6>
-                    <strong>Our Bikes</strong>
-                </h6>
-                <div className=" gap-4">
-                {cartsContent.map((card, index) => (
-            <CartCard
-                name={card.name}
-                photo={card.image}
-                href={card.href}
-                key={index}
-            />
-            ))}
-            </div>
-            {/* <Button
-            text="Manage Your Business"
-            buttonType="transparent"
-            margin="mt-8"
-            icon={faChevronRight}
-            /> */}
+      <h6>
+        <strong>Our Bikes</strong>
+      </h6>
+      <CartCardSwiper/>
     </section>
-  )
-}
+  );
+};
