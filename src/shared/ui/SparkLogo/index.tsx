c0d522a7aa849186647298dbbfd5c920f4038345
Ruslan Makhmatov <103198805+k1ngsterr1@users.noreply.github.com>
2024-06-06 00:32:07 +0600

    --VIDEO ADAPTED

--- src/shared/ui/SparkLogo/index.tsx
+++ src/shared/ui/SparkLogo/index.tsx
@@ -4,10 +4,12 @@ import Image from "next/image";
 
 import spark_logo from "@assets/vectors/spark_logo.svg";
 
+import styles from "./styles.module.scss";
+
 export const SparkLogo = () => {
   return (
     <Image
-      className="hoverable"
+      className={`hoverable ${styles.logo}`}
       src={spark_logo}
       onClick={() => (window.location.href = "https://sparkstudio.kz/home")}
       alt="Spark Studio Logotype"
