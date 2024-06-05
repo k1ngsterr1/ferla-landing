2be3e0961f46ddc05fc29c0628b22d780127e9a8
Ruslan Makhmatov <103198805+k1ngsterr1@users.noreply.github.com>
2024-06-06 00:48:29 +0600

    --SEND EMAIL & WEB-HOOK

--- src/shared/lib/hooks/useSendEmail.ts
+++ src/shared/lib/hooks/useSendEmail.ts
@@ -15,24 +15,36 @@ export function useSendEmail() {
     criteriaMode: "all",
   });
 
-  const onSubmit = (data: IData, event?: React.BaseSyntheticEvent) => {
+  const onSubmit = async (data: IData, event?: React.BaseSyntheticEvent) => {
     event?.preventDefault();
+
+    // Prepare the data for the webhook
+    const webhookUrl =
+      "https://hook.us1.make.com/p2o8bopvwaz7968bejo8khyelyv27ke1";
+    const postData = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
 
     const formData = data as unknown as Record<string, unknown>;
 
-    emailjs
-      .send(
-        process.env.NEXT_PUBLIC_SERVICE_ID!,
-        process.env.NEXT_PUBLIC_TEMPLATE_ID!,
-        formData,
-        process.env.NEXT_PUBLIC_PUBLIC_KEY!
-      )
-      .then((response) => {
-        console.log("Email sent successfully", response.text);
-      })
-      .catch((error) => {
-        console.error("Failed to send email", error.text);
-      });
+    emailjs.send(
+      process.env.NEXT_PUBLIC_SERVICE_ID!,
+      process.env.NEXT_PUBLIC_TEMPLATE_ID!,
+      formData,
+      process.env.NEXT_PUBLIC_PUBLIC_KEY!
+    );
+    // .then((response) => {
+    //   console.log("Email sent successfully", response.text);
+    // })
+    // .catch((error) => {
+    //   console.error("Failed to send email", error.text);
+    // });
+
+    const webhookResponse = await fetch(webhookUrl, postData);
   };
 
   return {
