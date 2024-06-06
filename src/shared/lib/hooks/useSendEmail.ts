00939bc32da8d74235886bff031c709bf6c33ffd
Ruslan Makhmatov <103198805+k1ngsterr1@users.noreply.github.com>
2024-06-06 21:56:27 +0600

    --NEXT ENV

--- src/shared/lib/hooks/useSendEmail.ts
+++ src/shared/lib/hooks/useSendEmail.ts
@@ -31,6 +31,7 @@ export function useSendEmail() {
 
     const formData = data as unknown as Record<string, unknown>;
 
+    // NEXT ENV
     emailjs.send(
       process.env.NEXT_PUBLIC_SERVICE_ID!,
       process.env.NEXT_PUBLIC_TEMPLATE_ID!,
