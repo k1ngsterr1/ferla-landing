3d61475e565fec6f562c7f6dcfcfb808c5a33301
senseiGai <swerss564@gmail.com>
2024-06-06 12:11:07 +0500

    --HEADER CHANGES

--- src/widgets/HomePage/Mobile/MainScreenMob/index.tsx
+++ src/widgets/HomePage/Mobile/MainScreenMob/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "@shared/ui/Button";
 import Link from "next/link";
-import { HeaderMobile } from "@features/HeaderMobile";   
+import { HeaderMobile } from "@features/HeaderMobile";
 
 import styles from "./styles.module.scss";
 
@@ -9,21 +9,22 @@ export const MainScreenMobile = () => {
   return (
     <main className={styles.main_screen_mob}>
       <div className={styles.main_screen_mob__content}>
-      <HeaderMobile/>
-      <h1 className={styles.main_screen_mob__content__heading}>
-        <strong>Business on Wheels for Everyone</strong>
-      </h1>
-      <p className={styles.main_screen_mob__content__paragraph}>
-        From Coffee Bikes and Ice Cream Bikes to Marketing & Promo Bikes, Hospitality Bikes, Vending Carts and more, we provide the perfect solution for your mobile retail pop-up.
-      </p>
-      <Button
-        text="Get A Free Consultation"
-        buttonType="filled"
-        margin="mt-8"
-      />
-      <Link className={styles.main_screen_mob__content__link} href="#video">
-        Watch the true story
-      </Link>
+        <h1 className={styles.main_screen_mob__content__heading}>
+          <strong>Business on Wheels for Everyone</strong>
+        </h1>
+        <p className={styles.main_screen_mob__content__paragraph}>
+          From Coffee Bikes and Ice Cream Bikes to Marketing & Promo Bikes,
+          Hospitality Bikes, Vending Carts and more, we provide the perfect
+          solution for your mobile retail pop-up.
+        </p>
+        <Button
+          text="Get A Free Consultation"
+          buttonType="filled"
+          margin="mt-8"
+        />
+        <Link className={styles.main_screen_mob__content__link} href="#video">
+          Watch the true story
+        </Link>
       </div>
     </main>
   );
