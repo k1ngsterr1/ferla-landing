0fbd99852f5fedd58b3a7017cff29160a94af3b4
senseiGai <swerss564@gmail.com>
2024-06-08 13:19:35 +0500

    POPUP ADAPTED MOBILE AND TABLET

--- src/features/CartCardSwiper/index.tsx
+++ src/features/CartCardSwiper/index.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { CartCard } from "@entities/CartCard";
 import { cartsContent } from "@shared/lib/content/cartsContent";
+import { PopupMobile } from "@entities/PopupMobile";
+import Link from "next/link";
 
 import styles from "./styles.module.scss";
 
@@ -11,25 +13,38 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-export const CartCardSwiper = () => {
+interface IOurBikesScreen {
+  allBikes: boolean;
+}
+
+export const CartCardSwiper: React.FC<IOurBikesScreen> = ({ allBikes }) => {
   return (
-    <Swiper
-      spaceBetween={20}
-      slidesPerView={1}
-      className={styles.swiper}
-      navigation={false}
-    >
-      {cartsContent.map((card, index) => (
-        <SwiperSlide key={index}>
-          <CartCard
-            name={card.name}
-            title={card.title}
-            paragraph={card.paragraph}
-            photo={card.image}
-            href={card.href}
-          />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <>
+      {allBikes && (
+        <Link
+          href="/products"
+          className={`${styles.bikes__upper__link} hoverable`}
+        ></Link>
+      )}
+      <Swiper
+        spaceBetween={20}
+        slidesPerView={1}
+        className={styles.swiper}
+        navigation={false}
+      >
+        {cartsContent.map((card, index) => (
+          <SwiperSlide key={index}>
+            <CartCard
+              name={card.name}
+              title={card.title}
+              paragraph={card.paragraph}
+              photo={card.image}
+              href={card.href}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <PopupMobile />
+    </>
   );
 };
