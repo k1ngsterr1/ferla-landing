0159f283c37b9e2e10e33d4b1028538a83083799
Ruslan Makhmatov <103198805+k1ngsterr1@users.noreply.github.com>
2024-06-13 20:57:09 +0600

    --UPDATED

--- src/shared/lib/hooks/useScript.ts
+++ src/shared/lib/hooks/useScript.ts
@@ -1,41 +1,23 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 
-const useScript = (src: string) => {
-  const [loaded, setLoaded] = useState(false);
-  const [error, setError] = useState(null);
-
+function useScript(src: string) {
   useEffect(() => {
-    let script = document.querySelector(`script[src="${src}"]`);
-
-    if (!script) {
-      script = document.createElement("script");
-      script.src = src;
-      script.async = true;
-      document.body.appendChild(script);
-
-      const handleLoad = () => {
-        setLoaded(true);
-      };
-
-      const handleError = () => {
-        setError(true);
-      };
-
-      script.addEventListener("load", handleLoad);
-      script.addEventListener("error", handleError);
-
-      return () => {
-        script.removeEventListener("load", handleLoad);
-        script.removeEventListener("error", handleError);
-      };
-    } else {
-      // If script already exists in the document, assume it's loaded
-      setLoaded(true);
+    // Check if the script is already loaded
+    if (document.querySelector(`script[src="${src}"]`)) {
+      return;
     }
-  }, [src]);
 
-  return { loaded, error };
-};
+    const script = document.createElement("script");
+    script.src = src;
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, [src]);
+}
 
 export default useScript;
