1397abd7c67009263488a0522873e9ca07ddd2d3
Ruslan Makhmatov <103198805+k1ngsterr1@users.noreply.github.com>
2024-06-13 21:22:12 +0600

    --UPDATED

--- src/widgets/HomePage/Mobile/LazyImageMobile/bikeseverywhere.tsx
+++ src/widgets/HomePage/Mobile/LazyImageMobile/bikeseverywhere.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image, { StaticImageData } from "next/image";
+import image from "@assets/webp/pc/bike_01_pc.webp";
 
 import { bikes } from "@shared/lib/content/bikesImagesContent";
 
@@ -14,11 +15,13 @@ const LazyImage: React.FC<ILazyImage> = ({ data }) => {
     <>
       {bikes.map((bike, index) => (
         <Image
+          width={950}
+          height={950}
           key={index}
           src={
             data.components && data.components[`${26 + index}`]
               ? data.component[`${26 + index}`].value
-              : "Content Not Found"
+              : image
           }
           priority
           className={styles.bikes_everywhere_mob__gallery_image}
