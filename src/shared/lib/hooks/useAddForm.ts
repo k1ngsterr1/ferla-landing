c955e538cb9df0933264c26fb7c48aaefd366f4d
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2024-06-18 14:00:19 +0500

    --UPDATED

--- src/shared/lib/hooks/useAddForm.ts
+++ src/shared/lib/hooks/useAddForm.ts
@@ -10,27 +10,16 @@ interface IData {
 export function useAddForm() {
   const addForm = async (data: IData): Promise<void> => {
     try {
+      const updatedData = {
+        ...data,
+        code: "SPARK-STUDIO-85209af2e07011fafd442671ef8ae84b647be17c7f517ea5942075dda6fbeeb7", // Укажите значение нового поля
+      };
+
       // Отправляем данные
       const response = await axios.post(
         "https://ferla-backend-production.up.railway.app/api/forms/add",
-        data
+        updatedData
       );
-
-      const formData = new FormData();
-
-      // Добавляем каждую часть данных в объект FormData
-      formData.append(
-        "code",
-        "SPARK-STUDIO-85209af2e07011fafd442671ef8ae84b647be17c7f517ea5942075dda6fbeeb7"
-      );
-      formData.append(
-        "url",
-        "https://ferla-backend-production.up.railway.app/api/forms/add"
-      );
-      formData.append("name", data.name);
-      formData.append("phoneNumber", data.phoneNumber);
-      formData.append("email", data.email);
-      formData.append("date", data.date);
 
       // Можно использовать formData для других целей, если требуется
       console.log(response.data);
