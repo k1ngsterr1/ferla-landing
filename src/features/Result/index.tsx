e5713ad2e4f9cf2c397be01dd4aa3997a556093d
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2024-06-19 14:30:00 +0500

    --CALCULATOR UPDATED

--- src/features/Result/index.tsx
+++ src/features/Result/index.tsx
@@ -11,11 +11,28 @@ export const Result: React.FC<IResultProps> = ({
   moneyBack,
   estimatedEarnings,
 }) => {
+  const getMoneyBackText = (value: number) => {
+    if (value >= 1 && value <= 1.5) {
+      return "1-2 months";
+    } else if (value > 1.5 && value <= 2) {
+      return "2-3 months";
+    } else if (value > 2) {
+      return "More than 3 months";
+    } else if (value < 1) {
+      return "Less than 1 month";
+    }
+
+    return value.toFixed(2);
+  };
+
   return (
     <div className={styles.result}>
       <div className="flex flex-col items-center">
         <span className={styles.result__money_back}>Make money back:</span>
-        <span className={styles.result__money_back_amount}>{moneyBack}</span>
+        <span className={styles.result__money_back_amount}>
+          {" "}
+          {getMoneyBackText(moneyBack)}
+        </span>
       </div>
       <div className="flex flex-col items-center">
         <span className={styles.result__money_back}>
